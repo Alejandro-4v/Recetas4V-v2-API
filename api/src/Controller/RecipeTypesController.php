adf60212450bd90fecd097711f53c885e771e04c
Alejacks <alejandrolopez2048@gmail.com>
2025-12-29 19:51:19 +0100

    feat: RecipeTypes controller updated to comply with OAS

--- api/src/Controller/RecipeTypesController.php
+++ api/src/Controller/RecipeTypesController.php
@@ -2,18 +2,16 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeTypeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class RecipeTypesController extends AbstractController
 {
-    #[Route('/recipe/types', name: 'app_recipe_types')]
-    public function index(): JsonResponse
+    #[Route('/recipe-types', name: 'app_recipe_types', methods: ['GET'])]
+    public function index(RecipeTypeRepository $recipeTypeRepository): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/RecipeTypesController.php',
-        ]);
+        return $this->json($recipeTypeRepository->findAll());
     }
 }
