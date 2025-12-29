0b9aaac198414fc012848a6bf179aca9adc0ba90
Alejacks <alejandrolopez2048@gmail.com>
2025-12-29 19:55:54 +0100

    feat: NutrientTypes controller updated to comply with OAS

--- api/src/Controller/NutrientTypesController.php
+++ api/src/Controller/NutrientTypesController.php
@@ -2,18 +2,16 @@
 
 namespace App\Controller;
 
+use App\Repository\NutrientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class NutrientTypesController extends AbstractController
 {
-    #[Route('/nutrient/types', name: 'app_nutrient_types')]
-    public function index(): JsonResponse
+    #[Route('/nutrient-types', name: 'app_nutrient_types', methods: ['GET'])]
+    public function index(NutrientRepository $nutrientRepository): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/NutrientTypesController.php',
-        ]);
+        return $this->json($nutrientRepository->findAll());
     }
 }
