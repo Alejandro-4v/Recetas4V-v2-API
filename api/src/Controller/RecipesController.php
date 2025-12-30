476301da31c544428312d22b5ded26530808b515
Alejacks <alejandrolopez2048@gmail.com>
2025-12-30 10:40:45 +0100

    feat: Recipes controller updated to comply with OAS's get method

--- api/src/Controller/RecipesController.php
+++ api/src/Controller/RecipesController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
+use Symfony\Component\HttpFoundation\Request;
+
 final class RecipesController extends AbstractController
 {
-    #[Route('/recipes', name: 'app_recipes')]
-    public function index(): JsonResponse
+    #[Route('/recipes', name: 'app_recipes', methods: ['GET'])]
+    public function index(RecipeRepository $recipeRepository, Request $request): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/RecipesController.php',
-        ]);
+
+        $type = $request->query->get('type');
+
+        if ($type) {
+            return $this->json($recipeRepository->findByType($type));
+        }
+
+        return $this->json($recipeRepository->findAll());
     }
 }
