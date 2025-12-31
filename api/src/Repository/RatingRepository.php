c62a8727bda90493724c183ba4b49b80bd832838
Alejacks <alejandrolopez2048@gmail.com>
2025-12-31 14:08:15 +0100

    feat: update RatingRepository to include findByRecipeId method and remove old comments

--- api/src/Repository/RatingRepository.php
+++ api/src/Repository/RatingRepository.php
@@ -16,28 +16,12 @@ class RatingRepository extends ServiceEntityRepository
         parent::__construct($registry, Rating::class);
     }
 
-    //    /**
-    //     * @return Rating[] Returns an array of Rating objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('r')
-    //            ->andWhere('r.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('r.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Rating
-    //    {
-    //        return $this->createQueryBuilder('r')
-    //            ->andWhere('r.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    public function findByRecipeId(int $recipeId) {
+        return $this->createQueryBuilder('r')
+            ->andWhere('r.recipe = :val')
+            ->setParameter('val', $recipeId)
+            ->orderBy('r.id', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
 }
